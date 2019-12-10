77abbfc35db97de76e8cedf0a0bd95bca25a0cc0
Jim Vanderveen <jvanderveen@ucdavis.edu>
2019-12-09 22:23:09 -0800

    complete puzzle for day 01 (part A)

---
+++ day01/fuel_req.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Read from given filename or stdin.
+# See https://unix.stackexchange.com/a/47543
+
+import argparse, sys
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', nargs='?')
+    args = parser.parse_args()
+    if args.filename:
+        # print('opening file ' + args.filename)
+        infile = open(args.filename, 'r')
+    else:
+        infile = sys.stdin
+
+    fuel_total = 0
+    for cnt, line in enumerate(infile):
+        mass = int(line)
+        fuel = Fuel_Needed(mass)
+        # print("mass {} requires {} fuel units".format(mass, fuel))
+        fuel_total += fuel
+
+    print("Total fuel required is {} fuel units".format(fuel_total))
+    
+    # If a file was opened, close it now.
+    if infile is not sys.stdin:
+        infile.close()
+
+
+def Fuel_Needed(mass):
+    # To find the fuel required for a module, take its mass, divide by
+    # three, round down, and subtract 2. But what if this results in a
+    # negative number?
+    return int(mass/3)-2
+
+
+if __name__ == '__main__':
+   main()
